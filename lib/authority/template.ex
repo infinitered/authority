62f7c4306ea2dad7ef0b6897b51032ce33a5a396
Daniel Berkompas <himself@danielberkompas.com>
2017-12-16 08:19:56 -0800

    Improve `Authority.Template` documentation

--- lib/authority/template.ex
+++ lib/authority/template.ex
@@ -1,190 +1,190 @@
 defmodule Authority.Template do
   @moduledoc """
-  `Authority` offers _implementations_, not just _behaviours_. Many
-  applications will be able to use a template instead of implementing
-  `Authority` behaviours manually.
+  `Authority` provides _implementations_, not just _behaviours_. Many apps will
+  be able to use a template instead of implementing `Authority` behaviours
+  manually.
 
-  ## Templates
+  ## Configuration
 
-  `Authority` ships with the following templates. All _existing_ templates
-  assume you are using Ecto for persistence. However, nothing about
-  `Authority`'s _behaviours_ require you to use `Ecto`.
+  `Authority.Template` takes two options:
 
-  ### Email/Password
-  _Basic email/password authentication only._
+  1. `:behaviours`: A list of behaviours to implement
+  2. `:config`: A list of options for those behaviours
 
-  Definition:
+  With this information, it will automatically implement the behaviours for
+  you.
 
-      defmodule MyApp.Accounts do
-        use Authentication.Template,
-          behaviours: [
-            Authority.Authentication
-          ],
-          config: [
-            repo: MyApp.Repo, 
-            user_schema: MyApp.Accounts.User
-          ]
-      end
-
-  Usage:
+    defmodule MyApp.Accounts do
+      use Authority.Template,
+        behaviours: [...],
+        config: [...]
+    end
     
-      MyApp.Accounts.authenticate({email, password})
-      # => {:ok, %MyApp.Accounts.User{}}
+  Each behaviour requires configuration settings.
 
-  Options:
+  #### Global Configuration
 
-  - `:repo`: (required) the `Ecto.Repo` to use for database lookups.
+  - `:repo`: (required) the `Ecto.Repo` to use for database lookups. All
+  `Authority` implementations currently assume you are using `Ecto`.
 
-  - `:user_schema`: (required) the `Ecto.Schema` that represents a user in your app.
+  Example:
 
-  - `:user_identity_field`: (optional) the identification field on `:user_schema`'s schema
-    (Default: `:email`)
+    defmodule MyApp.Accounts do
+      use Authority.Template,
+        behaviours: [...],
+        config: [repo: MyApp.Repo]
+    end
 
-  - `:user_password_field`: (optional) the password field `:user_schema`'s schema
-    (Default: `:encrypted_password`)
+  #### `Authority.Authentication`
+  _Provides basic email/password (or username/password) authentication._
+
+  - `:user_schema`: (required) the `Ecto.Schema` that represents a user in
+  your app.
+
+  - `:user_identity_field`: (optional) the identification field on
+  `:user_schema`'s schema (Default: `:email`)
+
+  - `:user_password_field`: (optional) the password field `:user_schema`'s
+  schema (Default: `:encrypted_password`)
 
   - `:user_password_algorithm`: (option) the password hashing algorithm
-    (Default: `:bcrypt`)
+  (Default: `:bcrypt`)
 
-  ### Email/Password + Locking
-  _Email/password authentication, plus automatic account locking after
-    a configurable number of attempts during a given time period._
+  Example:
 
-  Definition:
+    defmodule MyApp.Accounts do
+      use Authority.Template,
+        behaviours: [Authority.Authentication],
+        config: [
+          repo: MyApp.Repo,
+          user_schema: MyApp.Accounts.Schema
+        ]
+    end
 
-      defmodule MyApp.Accounts do
-        use Authentication.Template,
-          behaviours: [
-            Authority.Authentication,
-            Authority.Locking
-          ],
-          config: [
-            repo: MyApp.Repo,
-            user_schema: MyApp.Accounts.User,
-            lock_schema: MyApp.Accounts.Lock,
-            lock_attempt_schema: MyApp.Accounts.LoginAttempt,
-            lock_max_attempts: 5,
-            lock_interval_seconds: 120,
-            lock_duration_seconds: 6_000
-          ]
-      end
-
-  Usage
-
-      MyApp.Accounts.authenticate({email, password})
-      # => {:ok, %MyApp.Accounts.User{}}
-
-      # After too many failures
-      MyApp.Accounts.authenticate({"valid@email.com", "invalid"})
-      # => {:error, %MyApp.Accounts.Lock{reason: :too_many_attempts}}
-
-  Options:
+  #### `Authority.Locking`
+  _Provides automatic account locking after a configurable number of
+  attempts. Must be used with `Authority.Authentication`_.
 
   - `:lock_schema`: (required) the `Ecto.Schema` which represents a lock
 
-  - `:lock_attempt_schema`: (required) the `Ecto.Schema` which represents
-    a failed attempt to log in.
+  - `:lock_attempt_schema`: (required) the `Ecto.Schema` which represents a
+  failed attempt to log in.
 
   - `:lock_expiration_field`: (optional) the expiration field on the
-    `:lock_schema` schema (Default: `:expires_at`)
+  `:lock_schema` schema (Default: `:expires_at`)
 
   - `:lock_user_assoc`: (optional) the association on `:lock_schema` which
-    relates the lock to a user. (Default: `:user`)
+  relates the lock to a user. (Default: `:user`)
 
   - `:lock_reason_field`: (optional) the field on `:lock_schema`'s schema
-    which stores the reason for the lock. (Default: `:reason`)
+  which stores the reason for the lock. (Default: `:reason`)
 
   - `:lock_max_attempts`: (optional) the number of failed attempts that will
-    create a lock. (Default: `5`)
+  create a lock. (Default: `5`)
 
-  - `:lock_interval_seconds`: (optional) the interval in which attempts
-    are counted. For example, '5 failures in 10 minutes'. (Default: `6000`)
+  - `:lock_interval_seconds`: (optional) the interval in which attempts are
+  counted. For example, '5 failures in 10 minutes'. (Default: `6000`, 10
+  minutes)
 
   - `:lock_duration_seconds`: (optional) the duration that a user account
-    will be locked. (Default: `6000`)
+  will be locked. (Default: `6000`, 10 minutes)
 
-  ### Email/Password + Tokenization
-  _Email/password authentication, plus credential tokenization._
+  Example:
 
-  Definition:
+    defmodule MyApp.Accounts do
+      use Authority.Template,
+        behaviours: [
+          Authority.Authentication,
+          Authority.Locking
+        ],
+        config: [
+          repo: MyApp.Repo,
+          user_schema: MyApp.Accounts.User,
+          lock_schema: MyApp.Accounts.Lock,
+          lock_attempt_schema: MyApp.Accounts.LoginAttempt
+        ]
+    end
 
-      defmodule MyApp.Accounts do
-        use Authentication.Template,
-          behaviours: [
-            Authority.Authentication, 
-            Authority.Tokenization
-          ],
-          config: [
-            repo: MyApp.Repo,
-            user_schema: MyApp.Accounts.User,
-            token_schema: MyApp.Accounts.Token
-          ]
-      end
 
-  Usage:
-
-      MyApp.Accounts.authenticate({email, password})
-      # => {:ok, %MyApp.Accounts.User{}}
-
-      MyApp.Accounts.tokenize({email, password})
-      # => {:ok, %MyApp.Accounts.Token{}}
-
-  Options:
+  #### `Authority.Tokenization`
+  _Provides tokenization for credentials. Must be used with
+  `Authority.Authentication`_.
 
   - `:token_schema`: (required) the `Ecto.Schema` which represents a token.
 
   - `:token_field`: (optional) the field on `:token_schema` which stores the
-    token value. (Default: `:token`)
+  token value. (Default: `:token`)
 
   - `:token_user_assoc`: (optional) the association on `:token_schema` which
-    relates a token to a user. (Default: `:user`)
+  relates a token to a user. (Default: `:user`)
 
   - `:token_expiration_field`: (optional) the field on `:token_schema` which
-    stores the expiration date of the token. (Default: `:expires_at`)
+  stores the expiration date of the token. (Default: `:expires_at`)
 
   - `:token_purpose_field`: (optional) the field on `:token_schema` which
-    stores the purpose of the token. (Default: `:purpose`)
+  stores the purpose of the token. (Default: `:purpose`)
 
-  ### Email/Password + Locking + Tokenization
+  Example:
 
-  _Email/password authentication, plus credential tokenization._
+    defmodule MyApp.Accounts do
+      use Authority.Template,
+        behaviours: [
+          Authority.Authentication,
+          Authority.Tokenization
+        ],
+        config: [
+          repo: MyApp.Repo,
+          user_schema: MyApp.Accounts.User,
+          token_schema: MyApp.Accounts.Token
+        ]
+    end
 
-  Definition:
+  ## Usage
 
-      defmodule MyApp.Accounts do
-        use Authentication.Template,
-          behaviours: [
-            Authority.Authentication, 
-            Authority.Locking,
-            Authority.Tokenization
-          ],
-          config: [
-            repo: MyApp.Repo,
-            user_schema: MyApp.Accounts.User,
-            token_schema: MyApp.Accounts.Token,
-            lock_schema: MyApp.Accounts.Lock,
-            lock_attempt_schema: MyApp.Accounts.LoginAttempt
-          ]
+  Once you've configured your module, you can call `Authority` behaviour
+  functions, depending on the behaviours your chose.
+
+    alias MyApp.Accounts
+    
+    Accounts.authenticate({email, password})
+    # => {:ok, %MyApp.Accounts.User{}}
+    
+    Accounts.authenticate(%MyApp.Accounts.Token{token: "valid"})
+    # => {:ok, %MyApp.Accounts.User{}}
+    
+    Accounts.tokenize({email, password})
+    # => {:ok, %MyApp.Accounts.Token{}}
+    
+    # After too many failed attempts to log in:
+    Accounts.authenticate({email, password})
+    # => {:error, %MyApp.Accounts.Lock{reason: :too_many_attempts}}
+    
+    Accounts.tokenize({email, password})
+    # => {:error, %MyApp.Accounts.Lock{reason: :too_many_attempts}}
+    
+  ## Overriding
+
+  You can override any callback function to add support for new data types.
+  For example, you can override `identify` to provide support for custom
+  types.
+
+    defmodule MyApp.Accounts do
+      use Authority.Template,
+        behaviours: [Authority.Authentication],
+        config: [repo: MyApp.Repo, user_schema: MyApp.Accounts.User]
+        
+      def identify(%MyApp.CustomStruct{} = struct) do
+        # find user
       end
+      
+      def identify(other), do: super(other)
+    end
+    
+  ## Without Ecto
 
-  Usage:
-
-      MyApp.Accounts.authenticate({email, password})
-      # => {:ok, %MyApp.Accounts.User{}}
-
-      MyApp.Accounts.tokenize({email, password})
-      # => {:ok, %MyApp.Accounts.Token{}}
-
-      # Too many login attempts
-      MyApp.Accounts.authenticate({"valid@email.com", "invalid"})
-      # => {:error, %MyApp.Accounts.Lock{reason: :too_many_attempts}}
-
-      MyApp.Accounts.tokenize({"valid@email.com", "invalid"})
-      # => {:error, %MyApp.Accounts.Lock{reason: :too_many_attempts}}
-
-  Options:
-
-  _All of the options described above can be used._
+  `Authority.Template` assumes you are using `Ecto`. However, nothing about
+  the behaviours require you to use `Ecto`. You can simply implement the
+  behaviours manually without using `Authority.Template`.
   """
 
   alias Authority.{
