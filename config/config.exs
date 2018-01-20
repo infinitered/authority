0eac68ad59c338f8a89b5c8b8d9f170e6bd9d34c
Daniel Berkompas <himself@danielberkompas.com>
2018-01-19 16:32:40 -0800

    Remove unnecessary mix configuration

--- config/config.exs
+++ config/config.exs
@@ -1,14 +1,3 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
-
-config :logger, level: :info
-
-config :authority, ecto_repos: [Authority.Test.Repo]
-
-config :authority, Authority.Test.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  database: "authority_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  priv: "test/support/"
